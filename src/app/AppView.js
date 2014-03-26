392a8b2d283ff875d9025204fd1473e44b0b7221
Hongxu Liu <djshu.us@gmail.com>
2014-03-25 20:50:58 -0700

    Sliding page view left and right on toggle

--- src/app/AppView.js
+++ src/app/AppView.js
@@ -15,14 +15,39 @@ define(function(require, exports, module) {
     AppView.prototype = Object.create(View.prototype);
     AppView.prototype.constructor = AppView;
 
-    AppView.DEFAULT_OPTIONS = {};
+    AppView.DEFAULT_OPTIONS = {
+        transition: {
+            duration: 300,
+            curve: 'easeOut'
+        }
+    };
 
     function _createPageView() {
         this.pageView = new PageView();
+        this.pageView.on('menuToggle', this.toggleMenu.bind(this));
+
         this.pageModifier = new Modifier();
 
         this._add(this.pageModifier).add(this.pageView);
+        this.menuToggle = false;
     }
+
+    AppView.prototype.toggleMenu = function() {
+        if(this.menuToggle) {
+            this.slideLeft();
+        } else {
+            this.slideRight();
+        }
+        this.menuToggle = !this.menuToggle;
+    };
+
+    AppView.prototype.slideLeft = function() {
+        this.pageModifier.setTransform(Transform.translate(0, 0, 0), this.options.transition);
+    };
+
+    AppView.prototype.slideRight = function() {
+        this.pageModifier.setTransform(Transform.translate(276, 0, 0), this.options.transition);
+    };
 
     module.exports = AppView;
 });
